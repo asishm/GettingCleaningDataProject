8adfa4a9bf357c42832e4c046b8ea046935a4181
kluge-iitk <asishkm@gmail.com>
2015-06-19 23:25:53 +0530

    cleaned up steps according to project guidelines and added comments

--- run_analysis.R
+++ run_analysis.R
@@ -1,6 +1,19 @@
-# This section establishes variables for the paths to various files
-# Main_URL is the path to the data set folder
-# the other variables are the relative paths with respect to the data set folder
+############################################################
+### COURSE PROJECT: Getting and Cleaning Data            ###
+### AUTHOR: Asish Mahapatra                              ###
+### GITHUB: kluge-iitk                                   ###
+############################################################
+
+
+### Please refer to the README.md for instructions on how
+### to utilize this script
+
+## SETUP ##
+##################################################################################
+# This section establishes variables for the paths to various files              #
+# Main_URL is the path to the data set folder                                    #
+# the other variables are the relative paths with respect to the data set folder #
+##################################################################################
 
 Main_URL <- "./GettingCleaningData/UCI HAR Dataset"
 x_testurl <- "test/X_test.txt"
@@ -12,25 +25,28 @@ subtrainurl <- "train/subject_train.txt"
 featuresurl <- "features.txt"
 activityurl <- "activity_labels.txt"
 
-# load the main test and train data set and merge them
+##########################################################################
+## STEP1: Merges the training and the test sets to create one data set. ##
+##########################################################################
 
 x_test_data <- read.table(paste(Main_URL, x_testurl, sep = "/"))
 x_train_data <- read.table(paste(Main_URL, x_trainurl, sep = "/"))
 
 merged_data <- rbind(x_train_data, x_test_data)
 
-# read the feature names and assign the names to the merged data set
+# read the feature names
 
 features <- read.table(paste(Main_URL, featuresurl, sep = "/"))
 
-names(merged_data) <- features[[2]]
+#####################################################################################################
+## STEP 2: Extracts only the measurements on the mean and standard deviation for each measurement. ##
+## Used a filter that accepts only feature names that have "mean()" or "std()"                     ##
+#####################################################################################################
 
-# select those measurements that are either mean() or std() as required
-
-acceptedvars <- grepl("mean\\(\\)|std\\(\\)", names(merged_data))
+acceptedvars <- grepl("mean\\(\\)|std\\(\\)", features[[2]])
 merged_data <- merged_data[, acceptedvars]
 
-# load the subjects data, merge the test and train sets and add to the merged data set
+## load the subjects data, merge the test and train sets and add to the merged data set
 
 sub_test_data <- read.table(paste(Main_URL, subtesturl, sep = "/"))
 sub_train_data <- read.table(paste(Main_URL, subtrainurl, sep = "/"))
@@ -39,7 +55,10 @@ sub_data <- rbind(sub_train_data, sub_test_data)
 
 merged_data$subject <- sub_data$V1
 
-# load the test and train data for activity, merge them and load the activity labels
+#########################################################################################
+## STEP 3: Uses descriptive activity names to name the activities in the data set      ##
+## load the test and train data for activity, merge them and load the activity labels  ##
+#########################################################################################
 
 y_test_data <- read.table(paste(Main_URL, y_testurl, sep = "/"))
 y_train_data <- read.table(paste(Main_URL, y_trainurl, sep = "/"))
@@ -47,20 +66,30 @@ y_train_data <- read.table(paste(Main_URL, y_trainurl, sep = "/"))
 activitylabel <- read.table(paste(Main_URL, activityurl, sep = "/"))
 activities <- rbind(y_train_data, y_test_data)
 
-# define function to convert the numeric activity data to activity names
+#########################################################################################
+## define function to convert the numeric activity data to activity names              ##
+## add activity data to the merged data set and convert activity numeric data to names ##
+#########################################################################################
 
 num_to_label_activity <- function(n){as.character(activitylabel[n,2])}
-
-# add activity data to the merged data set and convert activity numeric data to names
-
 merged_data$activities <- num_to_label_activity(activities$V1)
+
+#########################################################################################
+## STEP 4: Appropriately labels the data set with descriptive variable names.          ##
+## Selects appropriate feature names and assigns them to names(merged_data)            ##
+#########################################################################################
+
+names(merged_data) <- c(as.character(features[[2]][acceptedvars]), "subject", "activities")
 
 library(dplyr)
 
-# group the merged data by activities and then by subject
+#########################################################################################
+## STEP 5: From the data set in step 4, creates a second, independent tidy data set    ##
+## with the average of each variable for each activity and each subject.               ##
+## group the merged data by activities and then by subject                             ##
+## summarise the grouped data and calculate the means of the other variables           ##
+## grouped by activity and subject                                                     ##
+#########################################################################################
 
 grouped_data <- group_by(merged_data, activities, subject)
-
-# summarise the grouped data and calculate the means of the other variables grouped by activity and subject
-
 final_data <- summarise_each(grouped_data, funs(mean))
